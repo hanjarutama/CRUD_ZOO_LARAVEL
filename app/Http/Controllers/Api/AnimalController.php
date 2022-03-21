b150afd95e41f1f4e3740787f0cd9ee260f8382c
hanjarutama <hanjarutama@gmail.com>
2022-03-21 22:51:05 +0700

    first commit

---
+++ app/Http/Controllers/Api/AnimalController.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+use App\Models\Animal;
+
+class AnimalController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $animal = Animal::all();
+
+        return response()->json([
+            'code' => 200,
+            'status' => true,
+            'massage' => "succes get all animal",
+            'data' => $animal
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $animal = Animal::create([
+            'nama' => $request->nama,
+            'umur' => $request->umur
+
+        ]);
+
+        return response()->json([
+            'code' => 200,
+            'status' => true,
+            'massage' => "succes create animal",
+            'data' => $animal
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Animal $animal)
+    {
+        return response()->json([
+            'code' => 200,
+            'status' => true,
+            'massage' => "succes get animal with id = ".$animal->id,
+            'data' => $animal
+        ]);
+    } 
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Animal $animal)
+    {
+        $animal->update([
+            'nama' => $request->nama,
+            'umur' => $request->umur
+        ]);
+        return response()->json([
+            'code' => 200,
+            'status' => true,
+            'massage' => "succes update animal with id = ".$animal->id,
+            'data' => $animal
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Animal $animal)
+    {
+        $animal->delete();
+        return response()->json([
+            'code' => 200,
+            'status' => true,
+            'massage' => "succes delete animal with id = ".$animal->id,
+            'data' => ""
+        ]);
+    }
+}
---
+++ app/Http/Controllers/Api/CageController.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+use App\Models\Cage;
+
+class CageController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $cage = Cage::all();
+
+        return response()->json([
+            'code' => 200,
+            'status' => true,
+            'massage' => "succes get all cage",
+            'data' => $cage
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $cage = Cage::create([
+            'namaCage' => $request->namaCage,
+            'description' => $request->description
+
+        ]);
+
+        return response()->json([
+            'code' => 200,
+            'status' => true,
+            'massage' => "succes create cage",
+            'data' => $cage
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Cage $cage)
+    {
+        return response()->json([
+            'code' => 200,
+            'status' => true,
+            'massage' => "succes get cage with id = ".$cage->id,
+            'data' => $cage
+        ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Cage $cage)
+    {
+        $cage ->update([
+            'namaCage' => $request->namaCage,
+            'description' => $request->description
+
+        ]);
+        return response()->json([
+            'code' => 200,
+            'status' => true,
+            'massage' => "succes update cage with id = ".$cage->id,
+            'data' => $cage
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Cage $cage)
+    {
+        $cage->delete();
+        return response()->json([
+            'code' => 200,
+            'status' => true,
+            'massage' => "succes delete cage with id = ".$cage->id,
+            'data' => ""
+        ]);
+    }
+}
